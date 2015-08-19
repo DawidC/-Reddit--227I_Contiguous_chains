b6330786699e2efaade0bfd24116b1dd1cbc36f3
Dawid Czarneta <dawid.code+github@gmail.com>
2015-08-19 18:25:56 +0200

    Do zrobienia !

---
+++ src/contiguous_chains.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Dawid Czarneta on 19.08.2015.
+ * https://www.reddit.com/r/dailyprogrammer/comments/3gpjn3/20150812_challenge_227_intermediate_contiguous/
+ */
+public class contiguous_chains {
+    public static void main(String[] args){
+        System.out.println("[Reddit] Challenge #227 Intermediate. Contiguous Chains");
+        
+    }
+}
